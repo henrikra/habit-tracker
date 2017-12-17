39568937ba856db47af62f3d843eeceee4b7309c
Areii1 <ari.jaaskelainen1@gmail.com>
2017-12-17 21:11:33 +0200

    add remove button to currentHabits

--- src/components/CurrentHabits.js
+++ src/components/CurrentHabits.js
@@ -7,18 +7,25 @@ class CurrentHabits extends Component {
     this.removeHabit = this.removeHabit.bind(this);
   }
 
-  removeHabit(habit) {
-    console.log(habit);
-    localStorage.removeItem(habit);
+  removeHabit(removeHabit) {
+    const filtederHabits = JSON.parse(localStorage.getItem("habits")).filter(habit => habit.name !== removeHabit.name);
+    const filteredHabitsStr = JSON.stringify(filtederHabits);
+    localStorage.setItem('habits', filteredHabitsStr);
+    console.log(localStorage.getItem('habits'));
+
   }
 
   render() {
-    const habitsList = JSON.parse(localStorage.getItem('habits')).map((key) =>
-    <div>
-      <h3>Habit: {key.name}</h3>
-      <p>updateFreq: {key.updateFreq}</p>
-      <p>validatingMethod: {key.validatingMethod}</p>
-      <button onClick={this.removeHabit}>Remove</button>
+    const habitsList = JSON.parse(localStorage.getItem('habits')).map((habit) =>
+    <div key={habit.name}>
+      <h3>Habit: {habit.name}</h3>
+      <p>updateFreq: {habit.updateFreq}</p>
+      <p>validatingMethod: {habit.validatingMethod}</p>
+      <button onClick={() => {
+        this.removeHabit(habit)
+        }}>
+        Remove
+      </button>
     </div>
   );
     return (
